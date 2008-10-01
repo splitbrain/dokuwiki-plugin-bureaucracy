27504716bc5d55be98746058ca17c5ccae94612b
Arne Pelka <mail@arnepelka.de>
2008-10-01 20:13:26 +0200

    German translation added
    darcs-hash:20081001181326-ce5f5-a2bb64761f70c542de519db63d08694828b4a63e.gz

---
+++ lang/de/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['e_unknowntype'] = 'Unbekannter Typ "%s"';
+$lang['e_missingargs'] = 'Nicht genug Argumente f&uuml;r %s %s';
+$lang['e_noaction']    = 'Keine Aktion definiert - wohin sollen die Daten gesendet werden?';
+$lang['e_mail']        = 'Beim Versenden der Daten ist ein Fehler augetreten';
+
+$lang['e_required']    = '"%s" muss ausgef&uuml;llt werden.';
+$lang['e_match']       = '"%s" wurde nicht korrekt ausgef&uuml;llt. (&uuml;berpr&uuml;ft gegen /%s/)';
+$lang['e_email']       = '"%s" muss eine g&uuml;ltige E-Mail Adresse sein.';
+$lang['e_numeric']     = '"%s" muss eine Nummer sein.';
+$lang['e_min']         = '"%s" muss gr&ouml;&szlig;er sein als %s.';
+$lang['e_max']         = '"%s" muss weniger sein als %s.';
+
+
+$lang['thanks']        = 'Ihre Daten wurden erfolgreich versandt. Vielen Dank.';
+$lang['mailsubject']   = 'Formulardaten &uuml;bermittelt auf %s';
+$lang['mailintro']     = 'Die folgenden Daten wurden &uuml;bermittelt am %s.';
