dc15fa97130d8f9a468e03dbb8ae13d3ff1c6e2c
Andreas Gohr <andi@splitbrain.org>
2009-01-03 01:40:15 +0100

    Support for namespace and sepchar in template action
    darcs-hash:20090103004015-7ad00-1393603b12c357e9b2605db8347355948de6139c.gz

--- actions/template.php
+++ actions/template.php
@@ -9,8 +9,12 @@ class syntax_plugin_bureaucracy_action_template extends syntax_plugin_bureaucrac
 
     function run($data, $thanks, $argv, $errors) {
         global $ID;
+        global $conf;
 
         $tpl = cleanID(array_shift($argv));
+        $ns  = cleanID(array_shift($argv));
+        $sep = array_shift($argv); // used for combining pagename parts
+        if(is_null($sep)) $sep = $conf['sepchar'];
 
         if(auth_quickaclcheck($tpl) < AUTH_READ){
             msg(sprintf($this->getLang('e_template'), $tpl), -1);
@@ -25,16 +29,21 @@ class syntax_plugin_bureaucracy_action_template extends syntax_plugin_bureaucrac
             return false;
         }
 
-        $sep = '_'; //FIXME
         $pagename = '';
-
 
         // run through fields and do replacements
         foreach($data as $opt) {
             $value = $_POST['bureaucracy'][$opt['idx']];
             $label = preg_quote($opt['label']);
             if(in_array($opt['cmd'],$this->nofield)) continue;
-            if($opt['pagename']) $pagename .= $sep . $value; // rmember values for pagename
+            // handle pagenames:
+            if($opt['pagename']){
+                // no namespace seperators in input allowed:
+                $name = $value;
+                if($conf['useslash']) $name = str_replace('/',' ',$name);
+                $name = str_replace(':',' ',$name);
+                $pagename .= $sep . $name;
+            }
             $pattern = '/@@' . $label . '@@/i';
             $template = preg_replace($pattern, $value, $template);
         }
@@ -45,6 +54,7 @@ class syntax_plugin_bureaucracy_action_template extends syntax_plugin_bureaucrac
             msg($this->getLang('e_pagename'), -1);
             return false;
         }
+        $pagename = $ns.':'.$pagename;
         if(page_exists($pagename)) {
             msg(sprintf($this->getLang('e_pageexists'), html_wikilink($pagename)), -1);
             return false;
