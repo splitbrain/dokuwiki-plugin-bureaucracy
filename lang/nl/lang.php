a337f10bd4e2406e2b6e1a5d4ffd7228b61a660e
Patrick van Beem <junk1@vanbeem.info>
2009-03-23 10:20:35 +0100

    Dutch translation
    Ignore-this: eb35213bb389b80c13c9f904f5d6fd6a
    
    darcs-hash:20090323092035-a262e-0d004f010045578d26dd09f88d0c0742e351fd1a.gz

---
+++ lang/nl/lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['e_unknowntype']   = 'Onbekend type "%s"';
+$lang['e_missingargs']   = 'Onvoldoende argumenten voor %s %s';
+$lang['e_noaction']      = 'Geen aktie opgegeven - waar moeten de gegevens heen?';
+$lang['e_mail']          = 'Het versturen van de gegevens is mislukt';
+
+$lang['e_required']      = '"%s" is verplicht';
+$lang['e_match']         = '"%s" heeft een ongeldige waarde. (Moet voldoen aan /%s/i)';
+$lang['e_email']         = '"%s" moet een geldig e-mail adres zijn.';
+$lang['e_numeric']       = '"%s" moet een nummer zijn.';
+$lang['e_min']           = '"%s" moet groter zijn dan %s.';
+$lang['e_max']           = '"%s" moet kleiner zijn dan %s.';
+
+$lang['e_pagename']      = 'Pagina naam ontbreekt.';
+$lang['e_pageexists']    = 'De pagina "%s" bestaat al. Kies een andere pagina naam.';
+$lang['e_template']      = 'Kon de template "%s" niet lezen. wellicht bestaat de pagina niet of zijn de rechten onvoldoende?';
+$lang['e_denied']        = 'Onvoldoende rechten om deze pagina te maken, niet ingelogd?';
+
+$lang['mailsubject']     = 'Forumulier gegevens verstuurd op %s';
+$lang['mailintro']       = 'De volgende gegevens zijn verstuurd op %s.';
+
+$lang['mail_thanks']     = 'De gegevens zijn verstuurt. Bedankt.';
+$lang['template_thanks'] = 'De pagina is aangemaakt, volg de link om hem te openen.';
+
+$lang['summary']         = 'Gemaakt door het formulier op %s';
