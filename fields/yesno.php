caadb82fbac8585f1c1c4fea95e77ebdbb95f983
Adrian Lang <mail@adrianlang.de>
2009-10-20 16:35:10 +0200

    Add hidden input for checkboxes
    darcs-hash:20091020143510-9bc07-e31d2b2d2b0fa3ffa86a0f5a60153151f6979c83.gz

--- fields/yesno.php
+++ fields/yesno.php
@@ -6,6 +6,7 @@ class syntax_plugin_bureaucracy_field_yesno extends syntax_plugin_bureaucracy_fi
         $params = array_merge(array('value' => false), $this->opt, $params);
         $params['check'] = $params['value'] ? 'checked="checked"' : '';
         $this->tpl = '<label class="@@CLASS@@"><span>@@LABEL@@</span>'.
+                     '<input type="hidden" name="@@NAME@@" value="0" />' .
                      '<input type="checkbox" name="@@NAME@@" value="Yes" @@CHECK@@ /></label>';
         parent::render($params, $form);
     }
