e9361c741852506a4964bc22dd16ae4186de4393
Adrian Lang <mail@adrianlang.de>
2009-10-21 15:16:15 +0200

    Rewrite Javascript.
    darcs-hash:20091021131615-9bc07-881245ba1c8c4c7897e3fa1b8d6b3526693f779b.gz

--- script.js
+++ script.js
@@ -1,47 +1,70 @@
-function update_status (val) {
-    if (val.cur.checked) {
-        var cur = true;
-    } else {
-        var cur = val.cur.value;
-    }
-    if (cur == val.target) {
-        val.fieldset.style.display = 'block';
-    } else {
-        val.fieldset.style.display = 'none';
-    }
-}
-
-function bind_val(func, val) {
-    return function () {
-        return func(val);
-    }
-}
+/**
+ * Handle display of dependent, i. e. optional fieldsets
+ *
+ * Fieldsets may be defined as dependent on the value of a certain input. In
+ * this case they contain a p element with the CSS class “bureaucracy_depends”.
+ * This p element holds a span with the class “bureaucracy_depends_fname”
+ * and optionally another span with “bureaucracy_depends_fvalue”. They
+ * specify the target input (fname) and the target value for which the fieldset
+ * is to be shown.
+ *
+ * This function adds onchange handlers to the relevant inputs for showing and
+ * heading the respective fieldsets.
+ *
+ * @author Adrian Lang <dokuwiki@cosmocode.de>
+ **/
 
 addInitEvent(function () {
     var form = $('bureaucracy__plugin');
-    if (!form) return;
+    if (!form) {
+        /* No form – gtfo. */
+        return;
+    }
     var depends = getElementsByClass('bureaucracy_depends', form, 'p');
-    if (!depends) return;
+    if (depends.length === 0) {
+        /* No fieldset with dependencies – gtfo. */
+        return;
+    }
+
+    /**
+     * onchange event handler
+     *
+     * This function changes the visibility of a fieldset depending on the
+     * value of the input specified by “this”. this.dpar contains the target
+     * value and the depending fieldset.
+     **/
+    function handle_update() {
+        this.dpar.fset.style.display = (this.checked || this.type !== 'checkbox' &&
+                                        (this.dpar.tval === true && this.value !== '') ||
+                                        this.value === this.dpar.tval) ? 'block' : 'none';
+    }
+    /* All labels in the form. */
     var labels = form.getElementsByTagName('label');
-    var dependencies = Array();
+
     for (var i = 0; i < depends.length ; ++i) {
-        var fname = getElementsByClass('bureaucracy_depends_fname', depends[i], 'span')[0].innerHTML;
-        var spans = getElementsByClass('bureaucracy_depends_fvalue', depends[i], 'span');
-        if (spans != '') {
-            var fvalue = spans[0].innerHTML;
-        } else {
-            var fvalue = true;
-        }
+        var fname = getElementsByClass('bureaucracy_depends_fname',
+                                       depends[i], 'span')[0].innerHTML;
+        var spans = getElementsByClass('bureaucracy_depends_fvalue',
+                                       depends[i], 'span');
+        var fvalue = (spans.length > 0) ? spans[0].innerHTML : true;
+
         for (var n = 0 ; n < labels.length ; ++n) {
             if (labels[n].firstChild.innerHTML === fname) {
                 break;
             }
         }
-        if (n == labels.length) return;
-        var dvalue = labels[n].getElementsByTagName('input')[0];
-        if (!dvalue) dvalue = labels[n].getElementsByTagName('select')[0];
-        dependencies[i] = {target: fvalue, cur: dvalue, fieldset: depends[i].parentNode};
-        update_status(dependencies[i]);
-        dependencies[i].cur.onchange = bind_val(update_status, dependencies[i]);
+        if (n === labels.length) return;
+
+        var tvalues = labels[n].getElementsByTagName('input');
+        if (tvalues.length === 0) tvalues = labels[n].getElementsByTagName('select');
+        if (tvalues.length === 0) return;
+
+        /* Get the input or select determining the visibility of this
+           fieldset. Take the last one to ignore the hidden checkbox input. */
+        var dvalue = tvalues[tvalues.length - 1];
+        dvalue.dpar = {fset: depends[i].parentNode, tval: fvalue};
+        dvalue.addEventListener('change', handle_update, false);
+        handle_update.apply(dvalue);
+        depends[i].style.display = 'none';
     }
 });
