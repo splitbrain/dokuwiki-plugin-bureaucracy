e8e986838f5c2281400b3806a1f4c406e28ee337
Adrian Lang <mail@adrianlang.de>
2009-11-23 13:32:10 +0100

    Output field name on non numeric error
    darcs-hash:20091123123210-9bc07-bd80e6e368c57ae7917049c204a137eae7f07058.gz

--- fields/number.php
+++ fields/number.php
@@ -7,7 +7,7 @@ class syntax_plugin_bureaucracy_field_number extends syntax_plugin_bureaucracy_f
         }
 
         if(!is_numeric($value)){
-            msg(sprintf($this->getLang('e_numeric'),hsc($opt['label'])),-1);
+            msg(sprintf($this->getLang('e_numeric'),hsc($this->getParam('label'))),-1);
             return false;
         }
         return true;
