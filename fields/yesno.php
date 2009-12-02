241fa7d8b011734b5092042e7d72c0aa1a81020c
Adrian Lang <mail@adrianlang.de>
2009-12-02 13:59:11 +0100

    Produce valid XHTML
    darcs-hash:20091202125911-9bc07-2684f97c56f3b52764a54ea3d4d1ea78b9682179.gz

--- fields/yesno.php
+++ fields/yesno.php
@@ -4,10 +4,11 @@ class syntax_plugin_bureaucracy_field_yesno extends syntax_plugin_bureaucracy_fi
 
     function render($params, $form) {
         $params = array_merge(array('value' => false), $this->opt, $params);
-        $params['check'] = $params['value'] ? 'checked="checked"' : '';
+        $check = $params['value'] ? 'checked="checked"' : '';
         $this->tpl = '<label class="@@CLASS@@"><span>@@LABEL@@</span>'.
                      '<input type="hidden" name="@@NAME@@" value="0" />' .
-                     '<input type="checkbox" name="@@NAME@@" value="Yes" @@CHECK@@ /></label>';
+                     '<input type="checkbox" name="@@NAME@@" value="Yes" ' .
+                     $check . ' /></label>';
         parent::render($params, $form);
     }
 }
