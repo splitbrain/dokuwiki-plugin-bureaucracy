64e83398f9878858748477a0ffbfaafcbc37bdea
Andreas Gohr <gohr@cosmocode.de>
2010-01-12 11:16:21 +0100

    length restriction for date fields

--- fields/date.php
+++ fields/date.php
@@ -3,7 +3,7 @@ require_once DOKU_PLUGIN . 'bureaucracy/fields/textbox.php';
 class syntax_plugin_bureaucracy_field_date extends syntax_plugin_bureaucracy_field_textbox {
     function __construct($syntax_plugin, $args) {
         parent::__construct($syntax_plugin, $args);
-        $this->tpl = form_makeTextField('@@NAME@@', '@@VALUE@@', '@@LABEL@@', '', '@@CLASS@@', array('class' => 'datepicker edit'));
+        $this->tpl = form_makeTextField('@@NAME@@', '@@VALUE@@', '@@LABEL@@', '', '@@CLASS@@', array('class' => 'datepicker edit','maxlength'=>'10'));
     }
 
     function handle_post($value) {
