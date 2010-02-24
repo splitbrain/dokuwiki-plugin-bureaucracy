3cf3bc0ba4e0df1abe0e9343440aa85b260e7613
Adrian Lang <lang@cosmocode.de>
2010-02-24 15:36:56 +0100

    Implement hidden fields via html fields

--- fields/hidden.php
+++ fields/hidden.php
@@ -1,12 +1,8 @@
 <?php
 class syntax_plugin_bureaucracy_field_hidden extends syntax_plugin_bureaucracy_field {
-    var $extraargs = 2;
-
     function render($params, $form) {
+        $this->_handlePreload();
+        $form->addHidden($params['name'], $this->getParam('value') . '');
         return;
-    }
-
-    function handle_post($value) {
-        return true;
     }
 }
