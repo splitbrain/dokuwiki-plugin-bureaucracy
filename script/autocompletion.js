8f6868a513f2bdccd0f63c18a50c7c3e165660a7
Adrian Lang <lang@cosmocode.de>
2010-03-12 11:26:39 +0100

    Factor out AutoCompletion JavaScript

---
+++ script/autocompletion.js
@@ -0,0 +1,89 @@
+/**
+ * Provides an asynchronously loaded autocompletion list for an input box.
+ * An action plugin should handle AJAX_CALL_UNKNOWN for the ajax call ajaxcall.
+ *
+ * @param input      DOMref                  The HTML input box
+ * @param ajaxcall   string                  The ajax request ›call‹ parameter
+ * @param multi      bool                    Whether the input can hold
+ *                                           multiple values
+ * @param prepare_li DOMref (DOMref, mixed)  Callback for styling the li from a
+ *                                           backend response list item; this
+ *                                           function should set the _value
+ *                                           property of the DOMref parameter
+ *
+ * @author Adrian Lang <lang@cosmocode.de>
+ */
+
+
+function AutoCompletion(input, ajaxcall, multi, prepare_li) {
+    if (typeof Delay === 'undefined') return;
+
+    var regex = multi ? /^((?:.*,)?)\s*([^,]*)$/
+                      : /^()(.*)$/;
+
+    function handle_click () {
+                this.parentNode._rm();
+                input.value = (input.value.replace(regex, '$1 ' + this._value)).match(/^\s*(.*)\s*$/)[1];
+                if (multi) {
+                    input.value += ', ';
+                }
+                input.focus();
+                return false;
+    }
+
+    var ajax = new sack(DOKU_BASE + 'lib/exe/ajax.php');
+    ajax.onCompletion = function () {
+        var autoid = input.name.replace(/[\[\]]/g, '') + '__auto';
+        var values = eval(this.response);
+        var oldul = $(autoid);
+        if (oldul) {
+            oldul._rm();
+        }
+        if (!values) return;
+
+        // Strip out already selected values
+        var n_values = [];
+        for (var value in values) {
+            var str = input.value.match(regex)[1];
+            do {
+                var index = str.indexOf(value);
+                if (index === -1) {
+                    n_values.push([value, values[value]]);
+                    break;
+                }
+                if ((index === 0 || str.charAt(index - 1).match(/[\s,]/)) &&
+                    (index + value.length === str.length || str.charAt(index + value.length).match(/[\s,]/))) {
+                    break;
+                }
+                str = str.slice(index + value.length);
+            } while (true);
+        }
+        if (n_values.length === 0) return;
+        values = n_values;
+
+        // Create list
+        var ul = document.createElement('ul');
+        ul.className = 'autocompletion ' + ajaxcall + '__auto';
+        ul.id = autoid;
+        ul.style.top = (findPosY(input) + input.offsetHeight - 1) + 'px';
+        ul.style.left = findPosX(input) + 'px';
+        ul.style.width = (input.offsetWidth - 10) + 'px';
+        ul._rm = function () { this.parentNode.removeChild(this); };
+
+        for (var index in values) {
+            var li = prepare_li(document.createElement('li'), values[index]);
+            addEvent(li, 'click', handle_click);
+            ul.appendChild(li);
+        }
+
+        input.parentNode.insertBefore(ul, input.nextSibling);
+    };
+
+    var delay = new Delay(function () {
+        ajax.setVar('call', ajaxcall);
+        ajax.setVar('search', this.value.match(regex)[2]);
+        ajax.runAJAX();
+    });
+
+    addEvent(input, 'keyup', function (e) { delay.start(this, e); });
+}
--- script/user.js
+++ script/user.js
@@ -4,92 +4,19 @@
  * @author Adrian Lang <lang@cosmocode.de>
  */
 addInitEvent(function () {
-    if(typeof Delay == 'undefined') return;
-
-    var regexes = {
-        'userpicker': /^()(.*)$/,
-        'userspicker': /^((?:.*,)?)\s*([^,]*)$/
-    };
-    var ajax = new sack(DOKU_BASE + 'lib/exe/ajax.php');
-    function manageResponse (input, autoid) {
-        var regex = regexes[input.className.match(/users?picker/)[0]];
-
-        var users = eval(this.response);
-        var oldul = $(autoid);
-        if (oldul) {
-            oldul.parentNode.removeChild(oldul);
-        }
-        if (!users) return;
-
-        // Strip out already selected user names
-        n_users = [];
-        for (var name in users) {
-            var str = input.value.match(regex)[1];
-            do {
-                var index = str.indexOf(name);
-                if (index === -1) {
-                    n_users.push([name, users[name]]);
-                    break;
-                }
-                if ((index === 0 || str.charAt(index - 1).match(/[\s,]/)) &&
-                    (index + name.length === str.length || str.charAt(index + name.length).match(/[\s,]/))) {
-                    break;
-                }
-                str = str.slice(index + name.length);
-            } while (true);
-        }
-        users = n_users;
-
-        if (users.length === 0) return;
-        var ul = document.createElement('ul');
-        ul.className = 'bureaucracy_user_auto';
-        ul.id = autoid;
-        ul.style.top = (findPosY(input) + input.offsetHeight - 1) + 'px';
-        ul.style.left = findPosX(input) + 'px';
-        ul.style.width = (input.offsetWidth - 10) + 'px';
-        for (var index in users) {
-            var name = users[index][0];
-            var fullname = users[index][1];
-            var li = document.createElement('li');
-            li.innerHTML = '<a href="#">' + fullname + ' (' + name + ')' + '</a>';
+    function prepare_li(li, value) {
+            var name = value[0];
+            li.innerHTML = '<a href="#">' + value[1] + ' (' + name + ')' + '</a>';
             li.id = 'bureaucracy__user__' + name.replace(/\W/g, '_');
-            li._user = name;
-            ul.appendChild(li);
-            addEvent(li, 'click', function () {
-                input.value = (input.value.replace(regex, '$1 ' + this._user)).match(/\s*(.*)\s*/)[1];
-                if (input.className.match(/userspicker/)) {
-                    input.value += ', ';
-                }
-                this.parentNode.parentNode.removeChild(this.parentNode);
-                input.focus();
-                return false;
-            });
+            li._value = name;
+            return li;
+    }
+
+    var classes = { 'userpicker': false, 'userspicker': true };
+    for (var c_class in classes) {
+        var pickers = getElementsByClass(c_class, document, 'input');
+        for (var i = 0 ; i < pickers.length ; ++i) {
+            AutoCompletion(pickers[i], 'bureaucracy_user_field', classes[c_class], prepare_li);
         }
-        input.parentNode.insertBefore(ul, input.nextSibling);
-    };
-
-    function handler () {
-        ajax.setVar('call', 'bureaucracy_user_field');
-        ajax.setVar('search', this.value.match(regexes[this.className.match(/users?picker/)[0]])[2]);
-        ajax.onCompletion = bind(manageResponse, this, this.name.replace(/[\[\]]/g, '') + '__auto');
-        ajax.runAJAX();
-    }
-
-    function event_handler (delay) {
-        return function (e) {
-            delay.start.call(delay, this, e);
-        };
-    }
-
-    var userpickers = getElementsByClass('userpicker', document, 'input');
-    for (var i = 0 ; i < userpickers.length ; ++i) {
-        var delay = event_handler(new Delay(handler));
-        addEvent(userpickers[i], 'keyup', delay);
-    }
-
-    userpickers = getElementsByClass('userspicker', document, 'input');
-    for (var i = 0 ; i < userpickers.length ; ++i) {
-        var delay = event_handler(new Delay(handler));
-        addEvent(userpickers[i], 'keyup', delay);
     }
 });
