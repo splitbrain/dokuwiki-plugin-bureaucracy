fd6b6254e0b1db23f85266d627fb6f7a0aa3e753
Adrian Lang <lang@cosmocode.de>
2010-03-23 15:29:39 +0100

    Add french translation by Ivan Pierre

---
+++ lang/fr/lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['e_unknowntype']   = 'Type inconu "%s"';
+$lang['e_missingargs']   = 'Pas assez d\'arguments pour %s %s';
+$lang['e_noaction']      = 'Pas d\'action d&eacute;finie - que faire des donn&eacute;es ?';
+$lang['e_mail']          = 'Erreur lors de l\'envoi des donn&eacute;es';
+$lang['e_unknownconstraint'] = 'Contrainte de champ inconue %s';
+
+$lang['e_required']      = '"%s" est requis';
+$lang['e_match']         = '"%s" n\'a pas &eacute;t&eacute; remplis correctement. (verifi&eacute; avec /%s/i)';
+$lang['e_email']         = '"%s" doit &ecirc;tre une adresse e-mail valide.';
+$lang['e_numeric']       = '"%s" doit &ecirc;tre un nombre.';
+$lang['e_date']          = '"%s" doit &ecirc;tre une date valide au format yyyy-mm-dd.';
+$lang['e_user']          = '"%s" doit &ecirc;tre le nom d\'un utilisateur existant.';
+$lang['e_users']         = '"%s" doit &ecirc;tre une liste de noms d\'utilisateurs s&eacute;par&eacute;s par des virgules.';
+$lang['e_min']           = '"%s" doit &ecirc;tre plus grand que %s.';
+$lang['e_max']           = '"%s" doit &ecirc;tre plus petit que %s.';
+
+$lang['e_pagename']      = 'Nom de page manquant.';
+$lang['e_pageexists']    = 'La page "%s" existe d&eacute;j&agrave;. Choisissez un autre nom.';
+$lang['e_template']      = 'Template "%s" introuvable. Il n\'existe pas ou vous n\'avez pas l\'autorisation de lecture.';
+$lang['e_denied']        = 'Vous n\'avez pas le droit de cr&eacute;er cette page, vous &ecirc;tes vous connect&eacute; ?';
+
+$lang['mailsubject']     = 'Donn&eacute;es du formulaire soumis &agrave; %s';
+$lang['mailintro']       = 'Les donn&eacute;es suivantes on &eacute;t&eacute; envoyées vers %s.';
+
+$lang['mail_thanks']     = 'Donn&eacute;es envoy&eacute;es avec succ&egrave;s. Merci.';
+$lang['template_thanks'] = 'La page a &eacute;t&eacute; cr&eacute;&eacute;e, suivez le lien pour l\'ouvrir.';
+
+$lang['summary']         = 'Cr&eacute;&eacute; depuis le formulaire %s';
+
+$lang['js']['month_names'][0] = 'Janvier';
+$lang['js']['month_names'][1] = 'Février';
+$lang['js']['month_names'][2] = 'Mars';
+$lang['js']['month_names'][3] = 'Avril';
+$lang['js']['month_names'][4] = 'Mai';
+$lang['js']['month_names'][5] = 'Juin';
+$lang['js']['month_names'][6] = 'Juillet';
+$lang['js']['month_names'][7] = 'Août';
+$lang['js']['month_names'][8] = 'Septembre';
+$lang['js']['month_names'][9] = 'Octobre';
+$lang['js']['month_names'][10] = 'Novembre';
+$lang['js']['month_names'][11] = 'Decembre';
+
+$lang['js']['weekdays'][0] = 'Dim';
+$lang['js']['weekdays'][1] = 'Lun';
+$lang['js']['weekdays'][2] = 'Mar';
+$lang['js']['weekdays'][3] = 'Mer';
+$lang['js']['weekdays'][4] = 'Jeu';
+$lang['js']['weekdays'][5] = 'Ven';
+$lang['js']['weekdays'][6] = 'Sam';
---
+++ lang/fr/settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['runas'] = 'Utilise les permissions de cet utilisateur (virtuel) en testant les ACLs pour lire et cr&eacute;er les pages de template.';
+
