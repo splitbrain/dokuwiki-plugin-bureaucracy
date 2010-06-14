0fbb3254d2ccd30c35499f7718db08400aebfcb1
Adrian Lang <lang@cosmocode.de>
2010-06-14 16:37:10 +0200

    Fix error message, add german translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,11 +4,15 @@ $lang['e_unknowntype']   = 'Unbekannter Typ "%s"';
 $lang['e_missingargs']   = 'Nicht genug Argumente für %s %s';
 $lang['e_noaction']      = 'Keine Aktion definiert - wohin sollen die Daten gesendet werden?';
 $lang['e_mail']          = 'Beim Versenden der Daten ist ein Fehler augetreten';
+$lang['e_unknownconstraint'] = 'Unbekannter Feld-Parameter %s';
 
 $lang['e_required']      = '"%s" muss ausgefüllt werden.';
 $lang['e_match']         = '"%s" wurde nicht korrekt ausgefüllt. (überprüft gegen /%s/)';
 $lang['e_email']         = '"%s" muss eine gültige E-Mail Adresse sein.';
 $lang['e_numeric']       = '"%s" muss eine Nummer sein.';
+$lang['e_date']          = '"%s" muss ein gültiges Datum im Format jjjj-mm-tt sein.';
+$lang['e_user']          = '"%s" muss der Name eines existierenden Benutzers sein.';
+$lang['e_users']         = '"%s" muss eine komma-separierte Liste existierender Benutzer sein.';
 $lang['e_min']           = '"%s" muss größer sein als %s.';
 $lang['e_max']           = '"%s" muss kleiner sein als %s.';
 
@@ -25,3 +29,23 @@ $lang['template_thanks'] = 'Die Seite wurde angelegt, folgen Sie dem Link um die
 
 $lang['summary']         = 'Created from the formular at %s';
 
+$lang['js']['month_names'][0] = 'Januar';
+$lang['js']['month_names'][1] = 'Februar';
+$lang['js']['month_names'][2] = 'März';
+$lang['js']['month_names'][3] = 'April';
+$lang['js']['month_names'][4] = 'Mai';
+$lang['js']['month_names'][5] = 'Juni';
+$lang['js']['month_names'][6] = 'Juli';
+$lang['js']['month_names'][7] = 'August';
+$lang['js']['month_names'][8] = 'September';
+$lang['js']['month_names'][9] = 'Oktober';
+$lang['js']['month_names'][10] = 'November';
+$lang['js']['month_names'][11] = 'Dezember';
+
+$lang['js']['weekdays'][0] = 'So';
+$lang['js']['weekdays'][1] = 'Mo';
+$lang['js']['weekdays'][2] = 'Di';
+$lang['js']['weekdays'][3] = 'Mi';
+$lang['js']['weekdays'][4] = 'Do';
+$lang['js']['weekdays'][5] = 'Fr';
+$lang['js']['weekdays'][6] = 'Sa';
