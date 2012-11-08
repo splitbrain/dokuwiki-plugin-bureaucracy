dba188bfe9423d994ad2df3485600c45571b348e
Klap-in <klapinklapin@gmail.com>
2012-11-09 00:22:55 +0100

    Refactored fieldsets.js to jQuery

--- script/fieldsets.js
+++ script/fieldsets.js
@@ -14,78 +14,52 @@
  * @author Adrian Lang <dokuwiki@cosmocode.de>
  **/
 
-addInitEvent(function () {
-    function handle(form) {
-        var depends = getElementsByClass('bureaucracy_depends', form, 'p');
-        if (depends.length === 0) {
-            /* No fieldset with dependencies – gtfo. */
-            return;
+jQuery(function () {
+
+    jQuery('form.bureaucracy__plugin').each(function () {
+
+        //look for p (with info about controller) in depending fieldsets
+        jQuery('p.bureaucracy_depends', this).each(function () {
+            //get controller info
+            var fname = jQuery(this).find('span.bureaucracy_depends_fname').html();
+            var fvalue = jQuery(this).find('span.bureaucracy_depends_fvalue');
+            fvalue = (fvalue.length ? fvalue.html() : true);
+
+            //get controller field and add info and change event to the input that controls depending fieldset
+            var fieldsetinfo = {
+                fset: jQuery(this).parent(),
+                tval: fvalue
+            };
+            jQuery("label").has(":first-child:contains('" + fname + "')").first()
+                .find("select,input:last").each(function () { //yesno field contains first a hidden input
+                    if(!jQuery(this).data('dparray')){
+                        jQuery(this).data('dparray', [fieldsetinfo]);
+                    }else{
+                        jQuery(this).data('dparray').push(fieldsetinfo)
+                    }
+                }).bind('change keyup', function () {
+                    updateFieldset(this);
+                }).change();
+
+        //hide p.bureaucracy_depends in fieldset
+        }).hide();
+
+        //show/hide fieldset and trigger depending children
+        function updateFieldset(input) {
+            jQuery.each(jQuery(input).data('dparray'), function (i, dp) {
+                dp.fset.toggle( input.parentNode.parentNode.style.display !== 'none' &&                      // input/checkbox is displayed AND
+                                ((input.checked == dp.tval)   ||                                             //  ( checkbox is checked
+                                  input.type   !== 'checkbox' && (dp.tval === true && input.value !== '') || //  OR no checkbox, but input is set
+                                  input.value  === dp.tval) );                                               //  OR input === dp.tval )
+
+                dp.fset.find('input,select').each(function () {
+                    if (jQuery(this).data('dparray')) {
+                        jQuery(this).change();
+                    }
+                })
+            });
         }
 
-        /**
-         * onchange event handler
-         *
-         * This function changes the visibility of a fieldset depending on the
-         * value of the input specified by “this”. this.dpar contains the target
-         * value and the depending fieldset.
-         **/
-        function handle_update() {
-            for (var n = 0 ; n < this.dpar.length ; ++n) {
-                this.dpar[n].fset.style.display = (this.parentNode.parentNode.style.display !== 'none' &&
-                                            ((this.checked == this.dpar[n].tval) || this.type !== 'checkbox' &&
-                                             (this.dpar[n].tval === true && this.value !== '') ||
-                                             this.value === this.dpar[n].tval)) ? 'block' : 'none';
-                var inputs = this.dpar[n].fset.getElementsByTagName('input');
-                for (var i = 0; i < inputs.length ; ++i) {
-                    if (inputs[i].dpar) handle_update.call(inputs[i]);
-                }
-                var inputs = this.dpar[n].fset.getElementsByTagName('select');
-                for (var i = 0; i < inputs.length ; ++i) {
-                    if (inputs[i].dpar) handle_update.call(inputs[i]);
-                }
-            }
-        }
 
-        /* All labels in the form. */
-        var labels = form.getElementsByTagName('label');
-
-        var todos = [];
-
-        for (var i = 0; i < depends.length ; ++i) {
-            var fname = getElementsByClass('bureaucracy_depends_fname',
-                                           depends[i], 'span')[0].innerHTML;
-            var spans = getElementsByClass('bureaucracy_depends_fvalue',
-                                           depends[i], 'span');
-            var fvalue = (spans.length > 0) ? spans[0].innerHTML : true;
-
-            for (var n = 0 ; n < labels.length ; ++n) {
-                if (labels[n].firstChild.innerHTML === fname) {
-                    break;
-                }
-            }
-            if (n === labels.length) return;
-
-            var tvalues = labels[n].getElementsByTagName('input');
-            if (tvalues.length === 0) tvalues = labels[n].getElementsByTagName('select');
-            if (tvalues.length === 0) return;
-
-            /* Get the input or select determining the visibility of this
-               fieldset. Take the last one to ignore the hidden checkbox input. */
-            var dvalue = tvalues[tvalues.length - 1];
-            if (!dvalue.dpar) dvalue.dpar = [];
-            dvalue.dpar.push({fset: depends[i].parentNode, tval: fvalue});
-            addEvent(dvalue, 'change', handle_update);
-            todos.push(dvalue);
-            depends[i].style.display = 'none';
-        }
-
-        for (var i = 0 ; i < todos.length ; ++i) {
-            handle_update.call(todos[i]);
-        }
-    }
-
-    var forms = getElementsByClass('bureaucracy__plugin', document, 'form');
-    for (var i = 0 ; i < forms.length ; ++i) {
-        handle(forms[i]);
-    }
+    });
 });
