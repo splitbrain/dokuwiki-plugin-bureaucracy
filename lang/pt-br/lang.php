f7a345906873a0069bcb6fe2c489225a77ef2ced
Benedito Carneiro <benedito.carneiro@gmail.com>
2013-01-09 20:43:18 +0100

    Portuguese Brazilian language update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,39 +1,39 @@
 <?php
 
-$lang['e_unknowntype']   = 'Tipo desconhecido "%s"';
-$lang['e_missingargs']   = 'Argumentos insuficientes para %s %s';
-$lang['e_noaction']      = 'Nenhuma a&ccedil;&atilde;o definida - para onde deveriam os dados ser enviados?';
-$lang['e_mail']          = 'Ocorreu algum erro com o envio dos dados';
-$lang['e_unknownconstraint'] = 'Restri&ccedil;&atilde;o %s de campo desconhecida';
+$lang['e_unknowntype']   = 'Tipo desconhecido "%s".';
+$lang['e_missingargs']   = 'Argumentos insuficientes para %s %s.';
+$lang['e_noaction']      = 'Nenhuma ação definida - para onde deveriam os dados ser enviados?';
+$lang['e_mail']          = 'Ocorreu algum erro com o envio dos dados.';
+$lang['e_unknownconstraint'] = 'Restrição %s de campo desconhecida.';
 
-$lang['e_required']      = '"%s" &eacute; requerido';
-$lang['e_match']         = '"%s" n&atilde;o foi corretamente preenchido. (Comparado com /%s/i)';
-$lang['e_email']         = '"%s" tem de ser um endere&ccedil;o de email v&aacute;lido.';
-$lang['e_numeric']       = '"%s" tem de ser um n&uacute;mero.';
-$lang['e_date']          = '"%s" tem de ser uma data v&aacute;lida no formato dd-mm-yyyy.';
-$lang['e_user']          = '"%s" tem de ser o nome de um usu&aacute;rio existente.';
-$lang['e_users']         = '"%s" tem de ser uma lista de nomes de usu&aacute;rios existentes separados por v&iacute;rgulas.';
+$lang['e_required']      = '"%s" é requerido.';
+$lang['e_match']         = '"%s" não foi corretamente preenchido. (Comparado com /%s/i).';
+$lang['e_email']         = '"%s" tem de ser um endereço de email válido.';
+$lang['e_numeric']       = '"%s" tem de ser um número.';
+$lang['e_date']          = '"%s" tem de ser uma data válida no formato dd-mm-yyyy.';
+$lang['e_user']          = '"%s" tem de ser o nome de um usuário existente.';
+$lang['e_users']         = '"%s" tem de ser uma lista de nomes de usuários existentes separados por vírgulas.';
 $lang['e_min']           = '"%s" tem de ser maior que %s.';
 $lang['e_max']           = '"%s" tem de ser menor que %s.';
 
-$lang['e_pagename']      = 'Falta o nome da p&aacute;gina.';
-$lang['e_pageexists']    = 'A p&aacute;gina "%s" j&aacute; existe. Escolha outro nome de p&aacute;gina.';
-$lang['e_template']      = 'O modelo "%s" n&atilde;o pode ser lido. Talvez n&atilde;o exista ou voc&ecirc; n&atilde;o tem permiss&atilde;o de l&ecirc;-lo?';
-$lang['e_denied']        = 'Voc&ecirc; n&atilde;o pode criar esta p&aacute;gina. Esqueceu-se de Entrar?';
+$lang['e_pagename']      = 'Falta o nome da página.';
+$lang['e_pageexists']    = 'A página "%s" já existe. Escolha outro nome de página.';
+$lang['e_template']      = 'O modelo "%s" não pode ser lido. Talvez não exista ou você não tem permissão de lê-lo?';
+$lang['e_denied']        = 'Você não pode criar esta página. Esqueceu-se de Entrar?';
 
-$lang['mailsubject']     = 'Formul&aacute;rio de dados submetido em %s';
+$lang['mailsubject']     = 'Formulário de dados submetido em %s.';
 $lang['mailintro']       = 'Os dados seguintes foram submetidos em %s.';
 
 $lang['mail_thanks']     = 'Seus dados foram enviados com sucesso. Obrigado.';
-$lang['template_thanks'] = 'A p&aacute;gina foi criada, clique no link para abr&iacute;-la.';
+$lang['template_thanks'] = 'A página foi criada, clique no link para abri-la.';
 
-$lang['summary']         = 'Criada a partir do formul&aacute;rio em %s';
+$lang['summary']         = 'Criada a partir do formulário em %s.';
 
 $lang['submit']         = 'Enviar';
 
 $lang['js']['month_names'][0] = 'Janeiro';
 $lang['js']['month_names'][1] = 'Fevereiro';
-$lang['js']['month_names'][2] = 'Mar&ccedil;o';
+$lang['js']['month_names'][2] = 'Março';
 $lang['js']['month_names'][3] = 'Abril';
 $lang['js']['month_names'][4] = 'Maio';
 $lang['js']['month_names'][5] = 'Junho';
@@ -44,10 +44,10 @@ $lang['js']['month_names'][9] = 'Outubro';
 $lang['js']['month_names'][10] = 'Novembro';
 $lang['js']['month_names'][11] = 'Dezembro';
 
-$lang['js']['weekdays'][0] = 'Seg';
-$lang['js']['weekdays'][1] = 'Ter';
-$lang['js']['weekdays'][2] = 'Qua';
-$lang['js']['weekdays'][3] = 'Qui';
-$lang['js']['weekdays'][4] = 'Sex';
-$lang['js']['weekdays'][5] = 'S&aacute;b';
-$lang['js']['weekdays'][6] = 'Dom';
+$lang['js']['weekdays'][0] = 'Dom';
+$lang['js']['weekdays'][1] = 'Seg';
+$lang['js']['weekdays'][2] = 'Ter';
+$lang['js']['weekdays'][3] = 'Qua';
+$lang['js']['weekdays'][4] = 'Qui';
+$lang['js']['weekdays'][5] = 'Sex';
+$lang['js']['weekdays'][6] = 'Sáb';
