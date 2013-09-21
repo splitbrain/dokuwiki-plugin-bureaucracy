d9edd6a04858b2e022d8f8a6999c4488b4cdda58
Gerrit Uitslag <klapinklapin@gmail.com>
2013-09-21 02:57:01 +0200

    Removed unused strings. It should be accepted by translation tool too

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -27,24 +27,3 @@ $lang['summary']         = 'Izveidots no veidlapas  %s';
 
 
 $lang['submit'] = 'Saglabāt';
-
-$lang['js']['month_names'][0] = 'janvāris';
-$lang['js']['month_names'][1] = 'februāris'; 
-$lang['js']['month_names'][2] = 'marts'; 
-$lang['js']['month_names'][3] = 'aprīlis'; 
-$lang['js']['month_names'][4] = 'maijs'; 
-$lang['js']['month_names'][5] = 'jūnijs'; 
-$lang['js']['month_names'][6] = 'jūlijs'; 
-$lang['js']['month_names'][7] = 'augusts'; 
-$lang['js']['month_names'][8] = 'septembris'; 
-$lang['js']['month_names'][9] = 'oktobris'; 
-$lang['js']['month_names'][10] = 'novembris'; 
-$lang['js']['month_names'][11] = 'decembris';
-
-$lang['js']['weekdays'][0] = 'Sv'; 
-$lang['js']['weekdays'][1] = 'P'; 
-$lang['js']['weekdays'][2] = 'O'; 
-$lang['js']['weekdays'][3] = 'T'; 
-$lang['js']['weekdays'][4] = 'C'; 
-$lang['js']['weekdays'][5] = 'Pk';
-$lang['js']['weekdays'][6] = 'S'; 
