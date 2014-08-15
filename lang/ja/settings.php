96ebc14880190ca2a72a9758864cd6af7839ae52
Hideaki SAWADA <chuno@live.jp>
2014-08-15 22:40:42 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,7 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['runas'] = 'テンプレートモードに影響があります。テンプレートを読み込み、ページを作成するために ACL チェックする場合、この（仮想）ユーザーの権限を使用してください。';
