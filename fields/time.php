b0a73b75ca0d13d37523d06efeb0be6b6f406299
Gerrit Uitslag <klapinklapin@gmail.com>
2014-11-04 14:18:18 +0100

    add a time field. Fixes #29

---
+++ fields/time.php
@@ -0,0 +1,42 @@
+<?php
+require_once DOKU_PLUGIN . 'bureaucracy/fields/textbox.php';
+
+/**
+ * Class syntax_plugin_bureaucracy_field_time
+ *
+ * A time in the format (h)h:mm(:ss)
+ */
+class syntax_plugin_bureaucracy_field_time extends syntax_plugin_bureaucracy_field_textbox {
+    /**
+     * Arguments:
+     *  - cmd
+     *  - label
+     *
+     * @param array $args The tokenized definition, only split at spaces
+     */
+    public function __construct($args) {
+        parent::__construct($args);
+        $attr = array(
+            'class' => 'edit',
+            'maxlength'=>'8'
+        );
+        if(!isset($this->opt['optional'])) {
+            $attr['required'] = 'required';
+        }
+        $this->tpl = form_makeTextField('@@NAME@@', '@@VALUE@@', '@@DISPLAY@@', '', '@@CLASS@@', $attr);
+    }
+
+    /**
+     * Validate field input
+     *
+     * @throws Exception when empty or wrong time format
+     */
+    protected function _validate() {
+        parent::_validate();
+
+        $value = $this->getParam('value');
+        if (!is_null($value) && !preg_match('/^\d{1,2}:\d{2}(?::\d{2})?$/', $value)) {
+            throw new Exception(sprintf($this->getLang('e_time'),hsc($this->getParam('display'))));
+        }
+    }
+}
