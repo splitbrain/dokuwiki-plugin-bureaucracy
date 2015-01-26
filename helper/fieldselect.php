5db49778473343bcf7b9b525143f711dcdaa4b2f
soakindulge <shzhuanghanjia@163.com>
2015-01-26 15:11:51 +0800

    Update fieldselect.php

--- helper/fieldselect.php
+++ helper/fieldselect.php
@@ -5,7 +5,7 @@
  * Creates a dropdown list
  */
 class helper_plugin_bureaucracy_fieldselect extends helper_plugin_bureaucracy_field {
-
+ 
     protected $mandatory_args = 3;
 
     /**
@@ -14,13 +14,12 @@ class helper_plugin_bureaucracy_fieldselect extends helper_plugin_bureaucracy_fi
      *  - label
      *  - option1|option2|etc
      *  - ^ (optional)
-     *  - selection1|selection2|etc#option1|option2|etc  
-     * 
+     * show1|show2|show3#real1|real2|real3
      * @param array $args The tokenized definition, only split at spaces
      */
     public function initialize($args) {
         $this->init($args);
-        $total_params=array_map('trim', explode('#',array_shift($args)));
+        $total_params=array_map('trim', explode('#',array_shift($args)));//edit by Hanjiazhuang. 2015.1.15
 		if (!isset($total_params[1])) 
 		    {$this->opt['args'] = array_map('trim', explode('|',$total_params[0]));
 			$this->opt['show_params'] = $this->opt['args'];
@@ -29,6 +28,8 @@ class helper_plugin_bureaucracy_fieldselect extends helper_plugin_bureaucracy_fi
 			{$this->opt['args'] = array_map('trim', explode('|',$total_params[1]));
 			 $this->opt['show_params'] = array_map('trim', explode('|',$total_params[0]));
 			}
+			
+		//$this->opt['args'] = array_map('trim', explode('|',array_shift($args)));edit by Hanjiazhuang. 2015.1.15
         $this->standardArgs($args);
         if (!isset($this->opt['value']) && isset($this->opt['optional'])) {
             array_unshift($this->opt['args'],' ');
@@ -54,13 +55,14 @@ class helper_plugin_bureaucracy_fieldselect extends helper_plugin_bureaucracy_fi
             $params['class'] = 'bureaucracy_error';
         }
         $params = array_merge($this->opt, $params);
-        $this->opt['show_params'] = array_combine($params['args'],$this->opt['show_params']);
+		$this->opt['show_params'] = array_combine($params['args'],$this->opt['show_params']);//edit by Hanjiazhuang. 2015.1.15
         $form->addElement(call_user_func_array('form_makeListboxField',
                                                 $this->_parse_tpl(
                                                     array(
                                                         '@@NAME@@',
-                                                        $this->opt['show_params'],
-                                                        '@@VALUE@@|' . $this->opt['show_params'][0],
+                                                        //$params['args'],
+														$this->opt['show_params'],//edit by Hanjiazhuang. 2015.1.15
+                                                        '@@VALUE@@|'.$this->opt['show_params'][0],//'@@VALUE|' . /*$params['args']*/$this->opt['show_params'][0] . '@@',//edit by Hanjiazhuang. 2015.1.15
                                                         '@@DISPLAY@@',
                                                         '@@ID@@',
                                                         '@@CLASS@@'
