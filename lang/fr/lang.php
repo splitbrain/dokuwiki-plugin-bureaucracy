2c5400e8ce7bef2579090af94666539d57957331
blacklord049 <vincent.lecomte@outlook.be>
2016-04-14 12:17:16 +0200

    Updated french strings

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -7,12 +7,12 @@ $lang['e_mail']          = 'Erreur lors de l\'envoi des donn&eacute;es';
 $lang['e_unknownconstraint'] = 'Contrainte de champ inconue %s';
 
 $lang['e_required']      = '"%s" est requis';
-$lang['e_match']         = '"%s" n\'a pas &eacute;t&eacute; remplis correctement. %s';
+$lang['e_match']         = '"%s" n\'a pas &eacute;t&eacute; saisi correctement. %s';
 $lang['checkagainst']    = '(verifi&eacute; avec /%s/i)';
 $lang['e_email']         = '"%s" doit &ecirc;tre une adresse e-mail valide.';
 $lang['e_numeric']       = '"%s" doit &ecirc;tre un nombre.';
 $lang['e_date']          = '"%s" doit &ecirc;tre une date valide au format yyyy-mm-dd.';
-$lang['e_user']          = '"%s" doit &ecirc;tre le nom d\'un utilisateur existant.';
+$lang['e_user']          = '"%s" doit &ecirc;tre un nom d\'un utilisateur existant.';
 $lang['e_users']         = '"%s" doit &ecirc;tre une liste de noms d\'utilisateurs s&eacute;par&eacute;s par des virgules.';
 $lang['e_min']           = '"%s" doit &ecirc;tre plus grand que %s.';
 $lang['e_max']           = '"%s" doit &ecirc;tre plus petit que %s.';
@@ -23,7 +23,7 @@ $lang['e_template']      = 'Template "%s" introuvable. Il n\'existe pas ou vous 
 $lang['e_denied']        = 'Vous n\'avez pas le droit de cr&eacute;er cette page, vous &ecirc;tes vous connect&eacute; ?';
 
 $lang['mailsubject']     = 'Donn&eacute;es du formulaire soumis &agrave; %s';
-$lang['mailintro']       = 'Les donn&eacute;es suivantes on &eacute;t&eacute; envoy&eacute;es vers %s.';
+$lang['mailintro']       = 'Les donn&eacute;es suivantes ont &eacute;t&eacute; envoy&eacute;es vers %s.';
 
 $lang['mail_thanks']     = 'Donn&eacute;es envoy&eacute;es avec succ&egrave;s. Merci.';
 $lang['template_thanks'] = 'La page a &eacute;t&eacute; cr&eacute;&eacute;e, suivez le lien pour l\'ouvrir.';
