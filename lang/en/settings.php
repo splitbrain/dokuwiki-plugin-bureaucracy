bc6092fc11626446bb0e1c32c0f3b70c125c4828
mentgit <mentgit@users.noreply.github.com>
2016-08-25 18:59:24 +0200

    Translation of config option templateoverwrite

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,4 +2,4 @@
 
 $lang['runas'] = 'Affects the template mode. Use this (virtual) user\'s permissions when checking ACLs for reading templates and creating pages.';
 $lang['maxEmailAttachmentSize'] = 'Max size of mail attachments in Bytes. (per File)';
-
+$lang['templateoverwrite'] = 'Overwrite an already existing page without warning.';
