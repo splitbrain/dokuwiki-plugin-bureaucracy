a1fd9cd45203475bb61792aa01393e5c66fd0a3a
Szymon Olewniczak <solewniczak@rid.pl>
2018-01-09 14:29:10 +0100

    class hierarchy

---
+++ helper/fieldmulticheck.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class helper_plugin_bureaucracy_fieldmultiselect
+ *
+ * Creates a multiselect box
+ */
+class helper_plugin_bureaucracy_fieldmulticheck extends helper_plugin_bureaucracy_fieldmultiselect {
+
+}
