11d388c9849836c112c058dab70731f22be62305
Thorsten Niederkrome <dralli@gmx.net>
2018-02-23 23:25:30 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,5 +1,8 @@
 <?php
 
-$lang['runas'] = 'Affects the template mode. Use this (virtual) user\'s permissions when checking ACLs for reading templates and creating pages.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['runas']                 = 'Affects the template mode. Use this (virtual) user\'s permissions when checking ACLs for reading templates and creating pages.';
 $lang['maxEmailAttachmentSize'] = 'Maximale größe eines Mail-Anhangs in Bytes (pro Datei).';
-
