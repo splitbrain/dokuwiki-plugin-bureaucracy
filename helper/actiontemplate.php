49b83858bc2f52b5fd85fcd1029aa758ff556e22
Jon Kuroda <jkuroda@eecs.berkeley.edu>
2018-05-16 13:29:42 -0700

    Add "overwrite" argument to "file" field type to allow overwriting file that already exists at the same namespace:filename
      * add "overwrite" to standardArgs()' list of known constraints in helper/field.php
      * add "overwrite" to helper_plugin_bureaucracy_fieldfile's initialization function
      * parameterize hardcoded "false" of media_save's $ow arg in processUploads() in helper/actiontemplate.php

--- helper/actiontemplate.php
+++ helper/actiontemplate.php
@@ -347,6 +347,7 @@ class helper_plugin_bureaucracy_actiontemplate extends helper_plugin_bureaucracy
             $label = $field->getParam('label');
             $file  = $field->getParam('file');
             $ns    = $field->getParam('namespace');
+            $ow    = $field->getParam('overwrite');
 
             //skip empty files
             if(!$file['size']) {
@@ -366,7 +367,7 @@ class helper_plugin_bureaucracy_actiontemplate extends helper_plugin_bureaucracy
             $res = media_save(
                 array('name' => $file['tmp_name']),
                 $id,
-                false,
+                $ow,
                 $auth,
                 $move);
 
