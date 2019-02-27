b3ccaa806f50eb819ed87ffc77ad0fdcb19e9c36
Schplurtz le Déboulonné <schplurtz@laposte.net>
2019-02-27 08:15:20 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,5 +5,5 @@
  *
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['runas']                 = 'Mode «template» : teste les ACL avec les permissions de cet utilisateur (virtuel) pour la lecture des page modèles et la création de page.';
+$lang['runas']                 = 'Mode «template» : teste les ACL avec les permissions de cet utilisateur (virtuel) pour la lecture des modèles et la création de page.';
 $lang['maxEmailAttachmentSize'] = 'Taille max en octet des pièces jointes dans les courriels. (par fichier)';
