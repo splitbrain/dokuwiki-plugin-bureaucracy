6f92b9c39a198178bc3a57cb687c30515f7b3889
Nima Hamidian <nima.hamidian@gmail.com>
2021-06-23 00:25:04 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nima Hamidian <nima.hamidian@gmail.com>
+ */
+$lang['e_unknowntype']         = 'نوع ناشناخته "%s" ';
+$lang['e_unknownaction']       = 'عمل ناشناخته "%s"';
+$lang['e_missingargs']         = 'آرگومان کافی برای %s  %s نیست';
+$lang['e_noaction']            = 'عملی تعریف نشده - داده‌ها کجا باید فرستاده شوند؟';
+$lang['e_mail']                = 'در رابطه با ارسال داده‌ها چیزی اشتباه شده است';
+$lang['e_unknownconstraint']   = 'قید فیلد ناشناخته %s';
+$lang['e_labelpage']           = 'صفحه برچسب %s یافت نشد';
+$lang['e_required']            = '"%s" لازم است';
+$lang['e_match']               = '"%s" به درستی پر نشد %s';
+$lang['checkagainst']          = '(نسبت به /%s/i بررسی شد)';
+$lang['e_email']               = 'لازمست "%s" یک نشانی ایمیل صحیح باشد.';
+$lang['e_numeric']             = '"%s" لازمست یک عدد باشد';
+$lang['e_date']                = '"%s" لازمست یک تاریخ صحیح به صورت yyyy-mm-dd  باشد';
+$lang['e_time']                = '"%s" لازمست یک زمان صحیح به صورت (h)h:mm(:ss) باشد.';
+$lang['e_user']                = '"%s" لازمست نام یک کاربر موجود باشد.';
+$lang['e_users']               = '"%s" لازمست لیست با کاما ازهم جداشده نامهای کاربران موجود باشد.';
+$lang['e_min']                 = '"%s" لازمست بزرگتر از %s باشد.';
+$lang['e_max']                 = '"%s" لازمست کوچکتر از %s باشد.';
+$lang['e_pagename']            = 'نام صفحه وارد نشده است.';
+$lang['e_pageexists']          = 'صفحه "%s" هم‌اکنون موجود است. لطفاً یک نام صفحه‌ی متفاوت انتخاب کنید.';
+$lang['e_template']            = ' ';
