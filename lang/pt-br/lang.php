d9159b0f5d2f2d8ae8873df9b39af9e69d716dee
Nilton Röhricht Junior <rohricht@gmail.com>
2022-07-28 12:40:29 +0200

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,34 +1,38 @@
 <?php
 
-$lang['e_unknowntype']   = 'Tipo desconhecido "%s".';
-$lang['e_missingargs']   = 'Argumentos insuficientes para %s %s.';
-$lang['e_noaction']      = 'Nenhuma ação definida - para onde deveriam os dados ser enviados?';
-$lang['e_mail']          = 'Ocorreu algum erro com o envio dos dados.';
-$lang['e_unknownconstraint'] = 'Restrição %s de campo desconhecida.';
-
-$lang['e_required']      = '"%s" é requerido.';
-$lang['e_match']         = '"%s" não foi corretamente preenchido. %s';
-$lang['checkagainst']    = '(Comparado com /%s/i)';
-$lang['e_email']         = '"%s" tem de ser um endereço de email válido.';
-$lang['e_numeric']       = '"%s" tem de ser um número.';
-$lang['e_date']          = '"%s" tem de ser uma data válida no formato dd-mm-yyyy.';
-$lang['e_time']          = '"%s" tem de ser um horário válido no formato (h)h:mm(:ss).';
-$lang['e_user']          = '"%s" tem de ser o nome de um usuário existente.';
-$lang['e_users']         = '"%s" tem de ser uma lista de nomes de usuários existentes separados por vírgulas.';
-$lang['e_min']           = '"%s" tem de ser maior que %s.';
-$lang['e_max']           = '"%s" tem de ser menor que %s.';
-
-$lang['e_pagename']      = 'Falta o nome da página.';
-$lang['e_pageexists']    = 'A página "%s" já existe. Escolha outro nome de página.';
-$lang['e_template']      = 'O modelo "%s" não pode ser lido. Talvez não exista ou você não tem permissão de lê-lo?';
-$lang['e_denied']        = 'Você não pode criar esta página. Esqueceu-se de Entrar?';
-
-$lang['mailsubject']     = 'Formulário de dados submetido em %s.';
-$lang['mailintro']       = 'Os dados seguintes foram submetidos em %s.';
-
-$lang['mail_thanks']     = 'Seus dados foram enviados com sucesso. Obrigado.';
-$lang['template_thanks'] = 'A página foi criada, clique no link para abri-la.';
-
-$lang['summary']         = 'Criada a partir do formulário em %s.';
-
-$lang['submit']         = 'Enviar';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nilton Röhricht Junior <rohricht@gmail.com>
+ */
+$lang['e_unknowntype']         = 'Tipo desconhecido "%s".';
+$lang['e_unknownaction']       = 'Ação desconhecida "%s"';
+$lang['e_missingargs']         = 'Argumentos insuficientes para %s %s.';
+$lang['e_noaction']            = 'Nenhuma ação definida - para onde deveriam os dados ser enviados?';
+$lang['e_mail']                = 'Ocorreu algum erro com o envio dos dados.';
+$lang['e_unknownconstraint']   = 'Restrição %s de campo desconhecida.';
+$lang['e_labelpage']           = 'Página de etiquetas "%s" não encontrada';
+$lang['e_required']            = '"%s" é requerido.';
+$lang['e_match']               = '"%s" não foi corretamente preenchido. %s';
+$lang['checkagainst']          = '(Comparado com /%s/i)';
+$lang['e_email']               = '"%s" tem de ser um endereço de email válido.';
+$lang['e_numeric']             = '"%s" tem de ser um número.';
+$lang['e_date']                = '"%s" tem de ser uma data válida no formato dd-mm-yyyy.';
+$lang['e_time']                = '"%s" tem de ser um horário válido no formato (h)h:mm(:ss).';
+$lang['e_user']                = '"%s" tem de ser o nome de um usuário existente.';
+$lang['e_users']               = '"%s" tem de ser uma lista de nomes de usuários existentes separados por vírgulas.';
+$lang['e_min']                 = '"%s" tem de ser maior que %s.';
+$lang['e_max']                 = '"%s" tem de ser menor que %s.';
+$lang['e_pagename']            = 'Falta o nome da página.';
+$lang['e_pageexists']          = 'A página "%s" já existe. Escolha outro nome de página.';
+$lang['e_template']            = 'O modelo "%s" não pode ser lido. Talvez não exista ou você não tem permissão de lê-lo?';
+$lang['e_denied']              = 'Você não pode criar esta página. Esqueceu-se de Entrar?';
+$lang['mailsubject']           = 'Formulário de dados submetido em %s.';
+$lang['mailintro']             = 'Os dados seguintes foram submetidos em %s.';
+$lang['mail_thanks']           = 'Seus dados foram enviados com sucesso. Obrigado.';
+$lang['template_thanks']       = 'A página foi criada, clique no link para abri-la.';
+$lang['summary']               = 'Criada a partir do formulário em %s.';
+$lang['attachmentMailEmpty']   = '(arquivo não enviado)';
+$lang['attachmentMailToLarge'] = '(arquivo muito grande para anexo de e-mail)';
+$lang['attachmentMailToLarge_userinfo'] = 'arquivo "%s" muito grande para anexo de e-mail (>%s) (mensagem enviada mesmo assim)';
+$lang['submit']                = 'Enviar';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nilton Röhricht Junior <rohricht@gmail.com>
+ */
+$lang['runas']                 = 'Afeta o modo de template. Use estas permissões de usuário (virtual) quando verificando ACLs para leitura de templates e criação de páginas.';
+$lang['maxEmailAttachmentSize'] = 'Tamanho máximo para anexos de e-mail em Bytes. (por arquivo)';
