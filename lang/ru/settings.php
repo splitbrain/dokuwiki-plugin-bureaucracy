7f1898f5ffdb758838b2af1de00b08644654ed34
Aleksandr Selivanov <alexgearbox@yandex.ru>
2024-01-27 20:45:29 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,7 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Yuriy Skalko <yuriy.skalko@gmail.com>
  */
-$lang['runas']                 = 'Использовать права (виртуального) пользователя при проверке доступа для чтения шаблонов и создания страниц.';
-$lang['maxEmailAttachmentSize'] = 'Макс. размер почтовых вложений в байтах. (на файл)';
+$lang['runas']                 = '(Влияет на режим шаблона.) Использовать права этого (виртуального) пользователя при проверке доступа для чтения шаблонов и создания страниц.';
+$lang['maxEmailAttachmentSize'] = 'Максимальный размер почтовых вложений в байтах. (на файл)';
