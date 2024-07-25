1e37c6f5f2f4495b4b158bdf029d445c32e237c7
weight loss drink <rolando_nation@yahoo.com>
2024-07-25 09:35:41 +0200

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,110 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author weight loss drink <rolando_nation@yahoo.com>
+ */
+$lang['e_unknowntype']         = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future 
+and it is time to be happy. I have read this post and if 
+I could I want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_unknownaction']       = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is 
+time to be happy. I have read this post and if I could I want to suggest you few interesting things or tips.
 
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
+Maybe you can write next articles referring to 
+this article. I wish to read more things about it!';
+$lang['e_missingargs']         = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be happy.
+I have read this post and if I could I want to suggest you few 
+interesting things or tips. Maybe you can write next articles 
+referring to this article. I wish to read more things about it!';
+$lang['e_noaction']            = 'Riva San Vitale';
+$lang['e_mail']                = 'rolando_nation@yahoo.com';
+$lang['e_unknownconstraint']   = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be happy.
+I have read this post and if I could I want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_labelpage']           = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time 
+to be happy. I have read this post and if I could I want to suggest you few interesting 
+things or tips. Maybe you can write next articles referring to this 
+article. I wish to read more things about it!';
+$lang['e_required']            = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time 
+to be happy. I have read this post and if I could I want to suggest 
+you few interesting things or tips. Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_match']               = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time 
+to be happy. I have read this post and if I could I want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
 
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
+I wish to read more things about it!';
+$lang['checkagainst']          = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it 
+is time to be happy. I have read this post and if I could I want to suggest you few 
+interesting things or tips. Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_email']               = 'rolando_nation@yahoo.com';
+$lang['e_numeric']             = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be 
+happy. I have read this post and if I could I want to 
+suggest you few interesting things or tips. Maybe 
+you can write next articles referring to 
+this article. I wish to read more things about it!';
+$lang['e_date']                = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be happy.
+I have read this post and if I could I want to suggest you few interesting things 
+or tips. Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_time']                = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be happy.
+I have read this post and if I could I want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_user']                = 'Rolando';
+$lang['e_users']               = 'Rolando';
+$lang['e_min']                 = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future 
+and it is time to be happy. I have read this post and if I 
+could I want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_max']                 = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be happy.
 
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
+I have read this post and if I could I want to suggest you few interesting things or tips.
 
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
-
-$lang['summary']         = 'Izveidots no veidlapas  %s';
-
-
-$lang['submit'] = 'Saglabāt';
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_pagename']            = 'Nation';
+$lang['e_pageexists']          = 'Rolando';
+$lang['e_template']            = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and 
+it is time to be happy. I have read this post and if I could I 
+want to suggest you few interesting things or tips.
+Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['e_denied']              = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be 
+happy. I have read this post and if I could I want to suggest you few interesting things 
+or tips. Maybe you can write next articles referring to this article.
+I wish to read more things about it!';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'rolando_nation@yahoo.com';
+$lang['mail_thanks']           = 'rolando_nation@yahoo.com';
+$lang['template_thanks']       = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss';
+$lang['summary']               = 'Rolando';
+$lang['attachmentMailEmpty']   = 'rolando_nation@yahoo.com';
+$lang['attachmentMailToLarge'] = 'rolando_nation@yahoo.com';
+$lang['attachmentMailToLarge_userinfo'] = 'rolando_nation@yahoo.com';
+$lang['submit']                = 'https://cardistry.wiki/index.php/The_Impact_Of_Protein_On_Weight_Loss
+It\'s the best time to make some plans for the future and it is time to be 
+happy. I have read this post and if I could I want to suggest 
+you few interesting things or tips. Maybe you can write next articles referring 
+to this article. I wish to read more things about it!';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author weight loss drink <rolando_nation@yahoo.com>
+ */
+$lang['runas']                 = 'Rolando';
+$lang['maxEmailAttachmentSize'] = 'rolando_nation@yahoo.com';
