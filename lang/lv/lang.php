54a531f4297cf6985b98da08240091325cfd1984
icebraker page 136 <milagrosackerman@yahoo.com.au>
2024-09-19 20:35:29 +0200

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,57 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
-
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
-
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
-
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
-
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
-
-$lang['summary']         = 'Izveidots no veidlapas  %s';
-
-
-$lang['submit'] = 'Saglabāt';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author icebraker page 136 <milagrosackerman@yahoo.com.au>
+ */
+$lang['e_unknowntype']         = 'It\'s awesome to go to see this website and reading the views of all mates concerning 
+this post, while I am also keen of getting experience.';
+$lang['e_unknownaction']       = 'It\'s awesome to go to see this website and reading the views of all mates concerning this post, while I am 
+also keen of getting experience.';
+$lang['e_missingargs']         = 'It\'s awesome to go to see this website and reading the views 
+of all mates concerning this post, while I am also keen of 
+getting experience.';
+$lang['e_noaction']            = 'Dale Of Walls';
+$lang['e_mail']                = 'milagrosackerman@yahoo.com.au';
+$lang['e_unknownconstraint']   = 'It\'s awesome to go to see this website and reading the views of all mates concerning this post, while I am also keen of getting experience.';
+$lang['e_labelpage']           = 'It\'s awesome to go to see this website and reading the 
+views of all mates concerning this post, while I 
+am also keen of getting experience.';
+$lang['e_required']            = 'It\'s awesome to go to see this website and reading the views of all mates concerning this post, while I am also keen of getting experience.';
+$lang['e_match']               = 'It\'s awesome to go to see this website and reading the views of all mates concerning this post, while I am also keen of getting experience.';
+$lang['checkagainst']          = 'It\'s awesome to go to see this website and reading the 
+views of all mates concerning this post, while I am also keen of getting experience.';
+$lang['e_email']               = 'milagrosackerman@yahoo.com.au';
+$lang['e_numeric']             = 'It\'s awesome to go to see this website and reading 
+the views of all mates concerning this post, while I am also keen of getting experience.';
+$lang['e_date']                = 'It\'s awesome to go to see this website and reading 
+the views of all mates concerning this post, while I am also keen of getting 
+experience.';
+$lang['e_time']                = 'It\'s awesome to go to see this website and reading the views of all 
+mates concerning this post, while I am also keen of getting experience.';
+$lang['e_user']                = 'Milagros';
+$lang['e_users']               = 'Milagros';
+$lang['e_min']                 = 'It\'s awesome to go to see this website and reading 
+the views of all mates concerning this post, while I am also keen of 
+getting experience.';
+$lang['e_max']                 = 'It\'s awesome to go to see this website and reading the views of all 
+mates concerning this post, while I am also keen of getting 
+experience.';
+$lang['e_pagename']            = 'Ackerman';
+$lang['e_pageexists']          = 'Milagros';
+$lang['e_template']            = 'It\'s awesome to go to see this website and reading the views of all mates concerning this post, while I am also 
+keen of getting experience.';
+$lang['e_denied']              = 'It\'s awesome to go to see this website and reading 
+the views of all mates concerning this post, while I am also keen of getting 
+experience.';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'milagrosackerman@yahoo.com.au';
+$lang['mail_thanks']           = 'milagrosackerman@yahoo.com.au';
+$lang['template_thanks']       = 'https://indiagovtjobsalert.com/category/sbi-ex-servicemen-recruitment/';
+$lang['summary']               = 'Milagros';
+$lang['attachmentMailEmpty']   = 'milagrosackerman@yahoo.com.au';
+$lang['attachmentMailToLarge'] = 'milagrosackerman@yahoo.com.au';
+$lang['attachmentMailToLarge_userinfo'] = 'milagrosackerman@yahoo.com.au';
+$lang['submit']                = 'It\'s awesome to go to see this website and reading the views of 
+all mates concerning this post, while I am also keen of getting experience.';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author icebraker page 136 <milagrosackerman@yahoo.com.au>
+ */
+$lang['runas']                 = 'Milagros';
+$lang['maxEmailAttachmentSize'] = 'milagrosackerman@yahoo.com.au';
