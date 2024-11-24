338b1474dcf293eb99107a472123d1dd12e1a26d
sell phones <alisonheisler@web.de>
2024-11-24 20:10:17 +0100

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,68 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author sell phones <alisonheisler@web.de>
+ */
+$lang['e_unknowntype']         = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info 
+an individual provide on your guests? Is going to be again regularly to check out new posts';
+$lang['e_unknownaction']       = 'I just could not depart your web site before suggesting that I actually enjoyed 
+the usual info an individual provide on your guests?
 
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
+Is going to be again regularly to check out new posts';
+$lang['e_missingargs']         = 'I just could not depart your web site before suggesting that 
+I actually enjoyed the usual info an individual provide 
+on your guests? Is going to be again regularly to check out 
+new posts';
+$lang['e_noaction']            = 'Hilversum';
+$lang['e_mail']                = 'alisonheisler@web.de';
+$lang['e_unknownconstraint']   = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info 
+an individual provide on your guests? Is going 
+to be again regularly to check out new posts';
+$lang['e_labelpage']           = 'I just could not depart your web site before suggesting that I actually 
+enjoyed the usual info an individual provide on your 
+guests? Is going to be again regularly to check out 
+new posts';
+$lang['e_required']            = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info 
+an individual provide on your guests? Is going to be again regularly to check out new posts';
+$lang['e_match']               = 'I just could not depart your web site before suggesting that 
+I actually enjoyed the usual info an individual provide on your 
+guests? Is going to be again regularly to check out new posts';
+$lang['checkagainst']          = 'I just could not depart your web site before suggesting that I actually enjoyed the usual 
+info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['e_email']               = 'alisonheisler@web.de';
+$lang['e_numeric']             = 'I just could not depart your web site before suggesting 
+that I actually enjoyed the usual info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['e_date']                = 'I just could not depart your web site before suggesting that I actually 
+enjoyed the usual info an individual provide on your guests?
 
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
-
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
-
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
-
-$lang['summary']         = 'Izveidots no veidlapas  %s';
-
-
-$lang['submit'] = 'Saglabāt';
+Is going to be again regularly to check out new posts';
+$lang['e_time']                = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['e_user']                = 'Alison';
+$lang['e_users']               = 'Alison';
+$lang['e_min']                 = 'I just could not depart your web site before suggesting that 
+I actually enjoyed the usual info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['e_max']                 = 'I just could not depart your web site before suggesting that I actually 
+enjoyed the usual info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['e_pagename']            = 'Heisler';
+$lang['e_pageexists']          = 'Alison';
+$lang['e_template']            = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info an individual provide on your 
+guests? Is going to be again regularly to check out new posts';
+$lang['e_denied']              = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info an individual provide on your guests?
+Is going to be again regularly to check out new posts';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'alisonheisler@web.de';
+$lang['mail_thanks']           = 'alisonheisler@web.de';
+$lang['template_thanks']       = 'https://chatgpt.com/';
+$lang['summary']               = 'Alison';
+$lang['attachmentMailEmpty']   = 'alisonheisler@web.de';
+$lang['attachmentMailToLarge'] = 'alisonheisler@web.de';
+$lang['attachmentMailToLarge_userinfo'] = 'alisonheisler@web.de';
+$lang['submit']                = 'I just could not depart your web site before suggesting that I actually enjoyed the usual info an individual provide on your 
+guests? Is going to be again regularly to check out new posts';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author sell phones <alisonheisler@web.de>
+ */
+$lang['runas']                 = 'Alison';
+$lang['maxEmailAttachmentSize'] = 'alisonheisler@web.de';
