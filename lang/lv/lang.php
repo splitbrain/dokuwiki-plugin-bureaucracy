6bcf060c64af49699d5618629d1d8aa702a3373e
Nhà thầu xây nhà trọn gói Bà Rịa <antoniainnes@bellsouth.net>
2024-11-30 03:35:46 +0100

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,131 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nhà thầu xây nhà trọn gói Bà Rịa <antoniainnes@bellsouth.net>
+ */
+$lang['e_unknowntype']         = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you wrote the guide in it or something.
 
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
+I think that you simply can do with a few percent to pressure the message home a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_unknownaction']       = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you wrote the guide in it or something.
+I think that you simply can do with a few percent to pressure the message home 
+a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_missingargs']         = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you wrote the 
+guide in it or something. I think that you simply can do with 
+a few percent to pressure the message home 
+a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_noaction']            = 'Piaseczno';
+$lang['e_mail']                = 'antoniainnes@bellsouth.net';
+$lang['e_unknownconstraint']   = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like 
+you wrote the guide in it or something. I think that you simply can do with a few percent to 
+pressure the message home a bit, but instead of that, that is wonderful 
+blog. A fantastic read. I\'ll definitely be back.
 
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_labelpage']           = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you 
+wrote the guide in it or something. I think that you simply can do with a few percent to pressure the message home a bit, 
+but instead of that, that is wonderful blog. A fantastic read.
+I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_required']            = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a 
+lot about this, like you wrote the guide in it or something.
 
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
+I think that you simply can do with a few percent to pressure the message home a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_match']               = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you 
+wrote the guide in it or something. I think that you simply can do 
+with a few percent to pressure the message home a bit, but instead 
+of that, that is wonderful blog. A fantastic read. I\'ll definitely be 
+back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['checkagainst']          = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, 
+like you wrote the guide in it or something. I think that you simply can do with a few percent to 
+pressure the message home a bit, but instead of that, 
+that is wonderful blog. A fantastic read. I\'ll 
+definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_email']               = 'antoniainnes@bellsouth.net';
+$lang['e_numeric']             = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this,
+like you wrote the guide in it or something. I think that you simply can do with a few percent to pressure 
+the message home a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_date']                = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot 
+about this, like you wrote the guide in it or something.
+I think that you simply can do with a few percent to pressure the 
+message home a bit, but instead of that, that is wonderful blog.
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_time']                = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a 
+lot about this, like you wrote the guide in it or 
+something. I think that you simply can do with a few percent to pressure the message home a bit, 
+but instead of that, that is wonderful blog. A fantastic read.
+I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_user']                = 'Antonia';
+$lang['e_users']               = 'Antonia';
+$lang['e_min']                 = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp 
+a lot about this, like you wrote the guide in it or something.
+I think that you simply can do with a few percent to pressure the message home a bit, but instead of that, that is wonderful blog.
 
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
+A fantastic read. I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_max']                 = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp 
+a lot about this, like you wrote the guide in it or something.
+I think that you simply can do with a few percent to pressure 
+the message home a bit, but instead of that, 
+that is wonderful blog. A fantastic read. I\'ll definitely be back.
 
-$lang['summary']         = 'Izveidots no veidlapas  %s';
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_pagename']            = 'Innes';
+$lang['e_pageexists']          = 'Antonia';
+$lang['e_template']            = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this,
+like you wrote the guide in it or something. I think that you simply can do with a few percent to pressure the message home a bit, but instead 
+of that, that is wonderful blog. A fantastic read.
+I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['e_denied']              = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this, like you 
+wrote the guide in it or something. I think that you simply can do with a few percent to pressure the 
+message home a bit, but instead of that, that 
+is wonderful blog. A fantastic read. I\'ll definitely be back.
 
 
-$lang['submit'] = 'Saglabāt';
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'antoniainnes@bellsouth.net';
+$lang['mail_thanks']           = 'antoniainnes@bellsouth.net';
+$lang['template_thanks']       = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['summary']               = 'Antonia';
+$lang['attachmentMailEmpty']   = 'antoniainnes@bellsouth.net';
+$lang['attachmentMailToLarge'] = 'antoniainnes@bellsouth.net';
+$lang['attachmentMailToLarge_userinfo'] = 'antoniainnes@bellsouth.net';
+$lang['submit']                = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Its like you read my thoughts! You seem to grasp a lot about this,
+like you wrote the guide in it or something. I think that you simply 
+can do with a few percent to pressure the message home a bit,
+but instead of that, that is wonderful blog. A fantastic read.
+I\'ll definitely be back.
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-ba-ria-vung-tau-thi-cong-xay-nha-tron-goi/';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nhà thầu xây nhà trọn gói Bà Rịa <antoniainnes@bellsouth.net>
+ */
+$lang['runas']                 = 'Antonia';
+$lang['maxEmailAttachmentSize'] = 'antoniainnes@bellsouth.net';
