29e96573393e99c06820334e09843e720d78fe41
Flexbud24 akcesoria malarskie <salvador.duffield@outlook.com>
2025-09-29 03:05:17 +0200

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,51 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
-
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
-
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
-
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
-
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
-
-$lang['summary']         = 'Izveidots no veidlapas  %s';
-
-
-$lang['submit'] = 'Saglabāt';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Flexbud24 akcesoria malarskie <salvador.duffield@outlook.com>
+ */
+$lang['e_unknowntype']         = 'What\'s up to all, it\'s genuinely a nice for me to go to 
+see this web site, it contains valuable Information.';
+$lang['e_unknownaction']       = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable Information.';
+$lang['e_missingargs']         = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable Information.';
+$lang['e_noaction']            = 'Fredrikstad';
+$lang['e_mail']                = 'salvador.duffield@outlook.com';
+$lang['e_unknownconstraint']   = 'What\'s up to all, it\'s genuinely a nice for me 
+to go to see this web site, it contains valuable Information.';
+$lang['e_labelpage']           = 'What\'s up to all, it\'s genuinely a nice for me to go to see 
+this web site, it contains valuable Information.';
+$lang['e_required']            = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable 
+Information.';
+$lang['e_match']               = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable 
+Information.';
+$lang['checkagainst']          = 'What\'s up to all, it\'s genuinely a nice for me to go 
+to see this web site, it contains valuable Information.';
+$lang['e_email']               = 'salvador.duffield@outlook.com';
+$lang['e_numeric']             = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable 
+Information.';
+$lang['e_date']                = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains 
+valuable Information.';
+$lang['e_time']                = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains 
+valuable Information.';
+$lang['e_user']                = 'Salvador';
+$lang['e_users']               = 'Salvador';
+$lang['e_min']                 = 'What\'s up to all, it\'s genuinely a nice for me to 
+go to see this web site, it contains valuable Information.';
+$lang['e_max']                 = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable 
+Information.';
+$lang['e_pagename']            = 'Duffield';
+$lang['e_pageexists']          = 'Salvador';
+$lang['e_template']            = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web site, it contains valuable Information.';
+$lang['e_denied']              = 'What\'s up to all, it\'s genuinely a nice for me to go to see this web 
+site, it contains valuable Information.';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'salvador.duffield@outlook.com';
+$lang['mail_thanks']           = 'salvador.duffield@outlook.com';
+$lang['template_thanks']       = 'http://bdsmstorys.com/tgpx/click.php?id=591&u=https://www.terzas.es/temas/tema000.php';
+$lang['summary']               = 'Salvador';
+$lang['attachmentMailEmpty']   = 'salvador.duffield@outlook.com';
+$lang['attachmentMailToLarge'] = 'salvador.duffield@outlook.com';
+$lang['attachmentMailToLarge_userinfo'] = 'salvador.duffield@outlook.com';
+$lang['submit']                = 'What\'s up to all, it\'s genuinely a nice for me to go 
+to see this web site, it contains valuable Information.';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Flexbud24 akcesoria malarskie <salvador.duffield@outlook.com>
+ */
+$lang['runas']                 = 'Salvador';
+$lang['maxEmailAttachmentSize'] = 'salvador.duffield@outlook.com';
