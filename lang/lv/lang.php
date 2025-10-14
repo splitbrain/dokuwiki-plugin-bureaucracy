35e224487060f3ea738fdd46320c04260b25fd85
Delilah <latashakeech@verizon.net>
2025-10-14 15:40:45 +0200

    Translation update (lv)

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -1,30 +1,98 @@
 <?php
 
-$lang['e_unknowntype']   = 'Nezināma tipa "%s"';
-$lang['e_missingargs']   = 'Pietrūkst argumentu %s %s';
-$lang['e_noaction']      = 'Nav definēta darbība, kur dati jāsūta vai jāievieto';
-$lang['e_mail']          = 'Kļūme nosūtot datus';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Delilah <latashakeech@verizon.net>
+ */
+$lang['e_unknowntype']         = 'You really make it appear so easy together with your presentation but I to find this matter to be really one thing that I believe 
+I would never understand. It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent 
+post, I\'ll try to get the hold of it!';
+$lang['e_unknownaction']       = 'You really make it appear so easy together with your presentation but I to find this matter to be really 
+one thing that I believe I would never understand.
 
-$lang['e_required']      = 'Jāaizpilda "%s" ';
-$lang['e_match']         = 'Nav pareizi aizpildīts "%s"  %s';
-$lang['checkagainst']    = '(neatbilst prasībai /%s/i)';
-$lang['e_email']         = '"%s" jābūt derīgai epasta adresei.';
-$lang['e_numeric']       = '"%s" jābūt skaitlim.';
-$lang['e_min']           = '"%s" jābūt lielākam par %s.';
-$lang['e_max']           = '"%s" jābūt mazākam par %s.';
+It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_missingargs']         = 'You really make it appear so easy together with your 
+presentation but I to find this matter to be really one thing 
+that I believe I would never understand. It sort of feels 
+too complicated and very vast for me. I am having a look forward 
+on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_noaction']            = 'Bywong';
+$lang['e_mail']                = 'latashakeech@verizon.net';
+$lang['e_unknownconstraint']   = 'You really make it appear so easy together with your presentation but I to find this matter to be 
+really one thing that I believe I would never understand.
 
-$lang['e_pagename']      = 'Nav lapas nosaukuma.';
-$lang['e_pageexists']    = 'Lapa "%s" jau eksistē. Lūdzu norādīt citu lapas vārdu.';
-$lang['e_template']      = 'Nevar nolasīt šablonu "%s". Varbūt tāda nav vai pietrūkst lasīšanas tiesību?';
-$lang['e_denied']        = 'Jums nav tiesību izveidot šo lapu, varbūt aizmirsāt ielogoties?';
+It sort of feels too complicated and very vast for me. I am 
+having a look forward on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_labelpage']           = 'You really make it appear so easy together with your presentation but I to find this matter to be really one thing that I 
+believe I would never understand. It sort of feels too complicated and 
+very vast for me. I am having a look forward on your subsequent post, I\'ll try 
+to get the hold of it!';
+$lang['e_required']            = 'You really make it appear so easy together with your presentation but 
+I to find this matter to be really one thing that I believe 
+I would never understand. It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_match']               = 'You really make it appear so easy together with your presentation but I 
+to find this matter to be really one thing that I 
+believe I would never understand. It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post, I\'ll try 
+to get the hold of it!';
+$lang['checkagainst']          = 'You really make it appear so easy together with your 
+presentation but I to find this matter to be really one thing that 
+I believe I would never understand. It sort of feels too 
+complicated and very vast for me. I am having a look forward on your subsequent post, I\'ll 
+try to get the hold of it!';
+$lang['e_email']               = 'latashakeech@verizon.net';
+$lang['e_numeric']             = 'You really make it appear so easy together with your presentation but I to find this matter 
+to be really one thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post, I\'ll try to get 
+the hold of it!';
+$lang['e_date']                = 'You really make it appear so easy together with your presentation but I to find this matter to be really one thing 
+that I believe I would never understand. It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post,
+I\'ll try to get the hold of it!';
+$lang['e_time']                = 'You really make it appear so easy together with your presentation but I to find this matter 
+to be really one thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me.
 
-$lang['mailsubject']     = 'Veidlapas dati nosūtīti uz %s';
-$lang['mailintro']       = 'Norādīties dati nosūtīti uz %s.';
+I am having a look forward on your subsequent post, I\'ll try 
+to get the hold of it!';
+$lang['e_user']                = 'Latasha';
+$lang['e_users']               = 'Latasha';
+$lang['e_min']                 = 'You really make it appear so easy together with your presentation but I to find this matter to be really one 
+thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post, I\'ll 
+try to get the hold of it!';
+$lang['e_max']                 = 'You really make it appear so easy together with your 
+presentation but I to find this matter to be really one thing that I believe I would never understand.
 
-$lang['mail_thanks']     = 'Jūsu dati veiksmīgi nosūtīti. Paldies!';
-$lang['template_thanks'] = 'Lapa izveidota, lai to atvērtu lieto šo saiti.';
+It sort of feels too complicated and very vast 
+for me. I am having a look forward on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_pagename']            = 'Keech';
+$lang['e_pageexists']          = 'Latasha';
+$lang['e_template']            = 'You really make it appear so easy together with your presentation but I to find this matter to 
+be really one thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me.
 
-$lang['summary']         = 'Izveidots no veidlapas  %s';
-
-
-$lang['submit'] = 'Saglabāt';
+I am having a look forward on your subsequent post, I\'ll try to get the hold of it!';
+$lang['e_denied']              = 'You really make it appear so easy together with your 
+presentation but I to find this matter to be really one thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me.
+I am having a look forward on your subsequent post,
+I\'ll try to get the hold of it!';
+$lang['mailsubject']           = 'DokuWiki Localization';
+$lang['mailintro']             = 'latashakeech@verizon.net';
+$lang['mail_thanks']           = 'latashakeech@verizon.net';
+$lang['template_thanks']       = 'https://www.photobooths.lk/magic-mirror-photo-booth/';
+$lang['summary']               = 'Latasha';
+$lang['attachmentMailEmpty']   = 'latashakeech@verizon.net';
+$lang['attachmentMailToLarge'] = 'latashakeech@verizon.net';
+$lang['attachmentMailToLarge_userinfo'] = 'latashakeech@verizon.net';
+$lang['submit']                = 'You really make it appear so easy together with your presentation but I 
+to find this matter to be really one thing that I believe I would never understand.
+It sort of feels too complicated and very vast for me. I am 
+having a look forward on your subsequent post, I\'ll try to get the hold of it!';
---
+++ lang/lv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Delilah <latashakeech@verizon.net>
+ */
+$lang['runas']                 = 'Latasha';
+$lang['maxEmailAttachmentSize'] = 'latashakeech@verizon.net';
