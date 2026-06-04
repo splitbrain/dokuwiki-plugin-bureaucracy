c77ff69c4c229e09a4dfded61ce108107b861169
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:15:42 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['e_unknowntype']         = 'Neznámy typ "%s"';
+$lang['e_unknownaction']       = 'Neznáma akcia "%s"';
+$lang['e_missingargs']         = 'Nedostatok argumentov pre %s %s';
+$lang['e_noaction']            = 'Nie je definovaná žiadna akcia - kam sa majú dáta odoslať?';
+$lang['e_mail']                = 'Pri odosielaní dát sa niečo pokazilo';
+$lang['e_unknownconstraint']   = 'Neznáme obmedzenie poľa %s';
+$lang['e_labelpage']           = 'Stránka s označeniami %s nebola nájdená';
+$lang['e_required']            = '"%s" je povinné';
+$lang['e_match']               = '"%s" nebolo vyplnené správne. %s';
+$lang['checkagainst']          = '(Kontrolované voči /%s/i)';
+$lang['e_email']               = '"%s" musí byť platná e-mailová adresa.';
+$lang['e_numeric']             = '"%s" musí byť číslo.';
+$lang['e_date']                = '"%s" musí byť platný dátum vo formáte yyyy-mm-dd.';
+$lang['e_time']                = '"%s" musí byť platný čas vo formáte (h)h:mm(:ss).';
+$lang['e_user']                = '"%s" musí byť meno existujúceho používateľa.';
+$lang['e_users']               = '"%s" musí byť čiarkami oddelený zoznam mien existujúcich používateľov.';
+$lang['e_min']                 = '"%s" musí byť väčšie než %s.';
+$lang['e_max']                 = '"%s" musí byť menšie než %s.';
+$lang['e_pagename']            = 'Chýba názov stránky.';
+$lang['e_pageexists']          = 'Stránka "%s" už existuje. Zvoľte prosím iný názov stránky.';
+$lang['e_template']            = 'Nepodarilo sa načítať šablónu "%s". Možno neexistuje alebo nemáte práva na čítanie?';
+$lang['e_denied']              = 'Nemáte oprávnenie vytvoriť túto stránku, možno ste sa zabudli prihlásiť?';
+$lang['mailsubject']           = 'Dáta formulára odoslané na %s';
+$lang['mailintro']             = 'Nasledujúce dáta boli odoslané na %s.';
+$lang['mail_thanks']           = 'Vaše dáta boli úspešne odoslané. Ďakujeme.';
+$lang['template_thanks']       = 'Stránka bola vytvorená, otvoríte ju kliknutím na odkaz.';
+$lang['summary']               = 'Vytvorené z formulára na %s';
+$lang['attachmentMailEmpty']   = '(súbor nebol odoslaný)';
+$lang['attachmentMailToLarge'] = '(súbor je príliš veľký pre e-mailovú prílohu)';
+$lang['attachmentMailToLarge_userinfo'] = 'súbor "%s" je príliš veľký pre e-mailovú prílohu (>%s) (e-mail bol napriek tomu odoslaný)';
+$lang['submit']                = 'Odoslať';
